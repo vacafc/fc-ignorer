265c57de5be5e2751824bb779158bc63368e38a6
vacafc <vacafc@users.noreply.github.com>
2020-12-18 13:34:25 +0100

    Remove threads and posts

--- js/ignorer.js
+++ js/ignorer.js
@@ -4,18 +4,36 @@ const PLACEHOLDER = 'ignore this value';
 const browser = chrome || browser;
 
 const ignoreUsers = () => {
-  const users = PLACEHOLDER;
+  const users = new Set(PLACEHOLDER);
   
-  const threads = [...document.getElementById('threadbits_forum_2').children];
+  const mainList = document.querySelectorAll('[id^="threadbits_forum_"]')[0];
+  if (mainList) {
+    const threads = [...mainList.children];
+    threads.forEach(thread => {
+      const author = thread.children[2].children[1].innerText.trim();
+      if (users.has(author)) {
+        thread.remove();
+      }
+    });
+  } else {
+    const postsList = document.getElementById('posts');
+    if (postsList) {
+      const posts = [...postsList.children];
+      posts.forEach(post => {
+        const author = post.getElementsByClassName('bigusername')[0].innerText.trim();
+        if (users.has(author)) {
+          post.remove();
+        }
+      });
+    }
+  }
 };
 
 browser.webNavigation.onCompleted.addListener(() => {
-  const users = ['a', 'b'];
-  browser.tabs.executeScript({
-      code: `(${ignoreUsers.toString().replace('PLACEHOLDER', JSON.stringify(users))})()`
-  });
-
   loadIgnored(users => {
-    
+    users.push('Udezehcnas');
+    browser.tabs.executeScript({
+      code: `(${ignoreUsers.toString().replace('PLACEHOLDER', JSON.stringify(users))})()`
+    });
   });
 }, {url: [{urlMatches : '.+://.+\\.forocoches\\.com/.+'}]});
